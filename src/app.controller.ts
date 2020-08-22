e296839f5d51a4a02f713249940d9020864de05f
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-08-22 23:30:21 +0200

    feat(Affirmations: mount resources)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Header, Post } from '@nestjs/common';
+import { AffirmationsService } from './affirmations/affirmations.service';
+import { TwilioService } from './twilio/twilio.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly affirmationsService: AffirmationsService,
+              private readonly twilioService: TwilioService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('/*')
+  @Header('Content-Type', 'text/html')
+  async getAffirmation(): Promise<string> {
+    const affirmation = await this.affirmationsService.getAffirmation();
+    return this.twilioService.say(affirmation);
   }
 }
